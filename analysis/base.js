07a2d39654e9f4cd527df8523ecdad264cd6a3a7
Dan Draper <dan@coderdan.co>
2020-12-26 22:35:09 +1100

    Added TypeAhead analyzer

--- analysis/base.js
+++ analysis/base.js
@@ -1,7 +1,23 @@
+const SipHash = require('siphash');
+const SIPKEY = [ 0xdeadbeef, 0xcafebabe, 0x8badf00d, 0x1badb002 ];
+
 module.exports = class Base {
   // TODO: Field max is 127 (or 7 bits)
   constructor(field) {
     this.field = field
+  }
+
+  /* Returns an 8-byte Buffer of the SIPHASHED term
+   * with a field header */
+  sipHashStrWithField(term) {
+    const {h: h, l: l} = SipHash.hash(SIPKEY, term);
+    const buff = Buffer.alloc(8);
+    buff.writeUint32BE(h)
+    buff.writeUint32BE(l, 4)
+
+    /* First byte is the field (7-bits) and the LSB of the first siphash byte */
+    buff.writeUint8((this.field << 1) + (buff.readUint8() & 1));
+    return buff.readBigUint64BE();
   }
 
   performForQuery(_predicate, _constraint) {
--- analysis/keyword.js
+++ analysis/keyword.js
@@ -1,20 +1,9 @@
-const SipHash = require('siphash');
-const SIPKEY = [ 0xdeadbeef, 0xcafebabe, 0x8badf00d, 0x1badb002 ];
 
 const Base = require('./base');
 
 class Keyword extends Base {
   perform(term) {
-    const {h: h, l: l} = SipHash.hash(SIPKEY, term);
-    const buff = Buffer.alloc(8);
-    buff.writeUint32BE(h)
-    buff.writeUint32BE(l, 4)
-
-    /* First byte is the field (7-bits) and the LSB of the first siphash byte */
-    buff.writeUint8((this.field << 1) + (buff.readUint8() & 1));
-
-    // TODO: ORE Encrypt (in the caller but not here)
-    return buff.readBigUint64BE();
+    return this.sipHashStrWithField(term);
   }
 
   // TODO: Test this
