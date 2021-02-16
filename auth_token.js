1d65ab4a7cbcfceffe8c0271bcad6d4d2b37ea82
Dan Draper <dan@coderdan.co>
2021-02-16 11:26:40 +1100

    Handle token auth rejections correctly

--- auth_token.js
+++ auth_token.js
@@ -10,13 +10,13 @@ const OPTS = {
   headers: {
     'Content-Type': 'application/x-www-form-urlencoded'
   }
-};
+}
 
 const DATA = {
   grant_type: "client_credentials"
 }
 
-/* Refresh tokens before the expiry to avouid API errors due
+/* Refresh tokens before the expiry to avoid API errors due
  * to race conditions. Expiry buffer is in seconds */
 const EXPIRY_BUFFER = 20
 
@@ -24,26 +24,29 @@ class AuthToken {
   /* 
    * Instantiates a new AuthToken.
    * @param {string} idpHost is the hostname of the issuing Identity Provider
-   * @param {string} dataServer the URL of the stash data service we want to connect to
    * @param {object} creds is an object containing the `clientId` and `clientSecret`
    */
-  constructor({idpHost, dataServer, creds}) {
+  constructor({idpHost, creds}) {
     this.request = {...OPTS}
     this.request.host = idpHost
     this.data = {...DATA}
     this.data.client_id = creds.clientId
     this.data.client_secret = creds.clientSecret
-    this.data.audience = dataServer
     this.token = null
     this.expiresAt = 0
   }
 
-  async getToken() {
+  /*
+   * Gets an Auth token for the given server. This will fail if access is denied.
+   *
+   * @param {string} dataServer the URL of the stash data service we want to connect to
+   * @returns {Promise} a promise for the token
+   */
+  async getToken(dataServer) {
     // Check if token is set and not expired
     // authenticate and return the token or just return the token
     if (!this.tokenValid()) {
-      console.log("Fetching new token")
-      const {access_token, expires_in} = await this.authenticate()
+      const {access_token, expires_in} = await this.authenticate(dataServer)
       this.token = access_token
       this.expiresAt = Math.trunc((new Date()).getTime() / 1000) + expires_in - EXPIRY_BUFFER
     }
@@ -54,13 +57,27 @@ class AuthToken {
     return !!this.token
   }
 
-  authenticate() {
-    const body = querystring.stringify(this.data)
+  /*
+   * Performs an OAuth2 exchange using a client credentials grant to the IdP
+   *
+   * @param {string} dataServer - the URL of the data server we want access to
+   */
+  authenticate(dataServer) {
+    const body = querystring.stringify({
+      audience: dataServer,
+      ...this.data
+    })
 
     return new Promise((resolve, reject) => {
       const req = https.request(this.request, (res) => {
-        res.on('data', (response) => {
-          resolve(JSON.parse(response))
+        res.on('data', (data) => {
+          const response = JSON.parse(data)
+
+          if (response.error) {
+            reject(response)
+          } else {
+            resolve(response)
+          }
         });
       });
 
