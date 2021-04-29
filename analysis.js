e6cfc206be12f14e3aaea3fc3fef7a5572ec2de0
Dan Draper <dan@coderdan.co>
2021-04-29 21:03:37 +1000

    Added Timestamp analyzer

--- analysis.js
+++ analysis.js
@@ -1,7 +1,8 @@
 
 const Analysis = {}
-Analysis.Keyword = require('./analysis/keyword');
-Analysis.UInt = require('./analysis/uint');
-Analysis.TypeAhead = require('./analysis/typeahead');
+Analysis.Keyword = require('./analysis/keyword')
+Analysis.UInt = require('./analysis/uint')
+Analysis.TypeAhead = require('./analysis/typeahead')
+Analysis.Timestamp = require('./analysis/timestamp')
 
 module.exports = Analysis
