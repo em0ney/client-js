ba4a05589d6399bb5f9f53db8dd089e36d2d5c52
Dan Draper <dan@coderdan.co>
2021-05-05 17:28:09 +1000

    Stick to 'true' literal for JS Client bool analyser

--- analysis/bool.test.js
+++ analysis/bool.test.js
@@ -8,72 +8,9 @@ describe('perform', () => {
     expect(result.readBigUInt64BE()).toEqual(1n)
   })
 
-  test('that the string "true" is analysed as truthy', () => {
-    const bool = new Bool()
-    const [result] = bool.perform("true")
-
-    expect(result.readBigUInt64BE()).toEqual(1n)
-  })
-
-  test('that the string "t" is analysed as truthy', () => {
-    const bool = new Bool()
-    const [result] = bool.perform("t")
-
-    expect(result.readBigUInt64BE()).toEqual(1n)
-  })
-
-  test('that the string "1" is analysed as truthy', () => {
-    const bool = new Bool()
-    const [result] = bool.perform("1")
-
-    expect(result.readBigUInt64BE()).toEqual(1n)
-  })
-
-  test('that the string "on" is analysed as truthy', () => {
-    const bool = new Bool()
-    const [result] = bool.perform("on")
-
-    expect(result.readBigUInt64BE()).toEqual(1n)
-  })
-
-  test('that the literal 1 is analysed as truthy', () => {
-    const bool = new Bool()
-    const [result] = bool.perform(1)
-
-    expect(result.readBigUInt64BE()).toEqual(1n)
-  })
-
-  test('that the string "false" is analysed as falsy', () => {
-    const bool = new Bool()
-    const [result] = bool.perform("false")
-
-    expect(result.readBigUInt64BE()).toEqual(0n)
-  })
-
-  test('that the string "f" is analysed as falsy', () => {
-    const bool = new Bool()
-    const [result] = bool.perform("f")
-
-    expect(result.readBigUInt64BE()).toEqual(0n)
-  })
-
   test('that the literal false is analysed as falsy', () => {
     const bool = new Bool()
     const [result] = bool.perform(false)
-
-    expect(result.readBigUInt64BE()).toEqual(0n)
-  })
-
-  test('that the string "off" is analysed as falsy', () => {
-    const bool = new Bool()
-    const [result] = bool.perform("off")
-
-    expect(result.readBigUInt64BE()).toEqual(0n)
-  })
-
-  test('that the string "0" is analysed as falsy', () => {
-    const bool = new Bool()
-    const [result] = bool.perform("0")
 
     expect(result.readBigUInt64BE()).toEqual(0n)
   })
