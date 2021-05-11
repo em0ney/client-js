ff4d9a0d8f0dd3ac88556c2a6b363c49295d5e99
Dan Draper <dan@coderdan.co>
2021-05-11 16:30:00 +1000

    Correctly named the timestamp module in tests

--- analysis/utc-timestamp.test.js
+++ analysis/utc-timestamp.test.js
@@ -1,7 +1,7 @@
-const Timestamp = require('./utc-timestamp');
+const UTCTimestamp = require('./utc-timestamp');
 
 test('a Date object is encoded as a 64-bit buffer', () => {
-  const timestamp = new Timestamp()
+  const timestamp = new UTCTimestamp()
   const [result] = timestamp.perform(new Date("2021-04-29T05:54:39.285Z"))
 
   expect(result.readBigUInt64BE()).toEqual(1619675679285n)
@@ -9,7 +9,7 @@ test('a Date object is encoded as a 64-bit buffer', () => {
 
 describe('Comparisons', () => {
   test('that two dates in different years compare correctly', () => {
-    const timestamp = new Timestamp()
+    const timestamp = new UTCTimestamp()
     const [a] = timestamp.perform(new Date(2021, 3, 29, 5, 54, 39, 285))
     const [b] = timestamp.perform(new Date(2020, 3, 29, 5, 54, 39, 285))
 
@@ -17,7 +17,7 @@ describe('Comparisons', () => {
   })
 
   test('that two dates in different months of the same year compare correctly', () => {
-    const timestamp = new Timestamp()
+    const timestamp = new UTCTimestamp()
     const [a] = timestamp.perform(new Date(2021, 3, 29, 5, 54, 39, 285))
     const [b] = timestamp.perform(new Date(2021, 4, 29, 5, 54, 39, 285))
 
@@ -25,7 +25,7 @@ describe('Comparisons', () => {
   })
 
   test('that two dates in different days of the same month compare correctly', () => {
-    const timestamp = new Timestamp()
+    const timestamp = new UTCTimestamp()
     const [a] = timestamp.perform(new Date(2021, 4, 25, 5, 54, 39, 285))
     const [b] = timestamp.perform(new Date(2021, 4, 29, 5, 54, 39, 285))
 
@@ -33,7 +33,7 @@ describe('Comparisons', () => {
   })
 
   test('that two dates in different hours of the same day compare correctly', () => {
-    const timestamp = new Timestamp()
+    const timestamp = new UTCTimestamp()
     const [a] = timestamp.perform(new Date(2021, 4, 25, 8, 54, 39, 285))
     const [b] = timestamp.perform(new Date(2021, 4, 25, 6, 54, 39, 285))
 
@@ -41,7 +41,7 @@ describe('Comparisons', () => {
   })
 
   test('that two dates in different minutes of the same hour compare correctly', () => {
-    const timestamp = new Timestamp()
+    const timestamp = new UTCTimestamp()
     const [a] = timestamp.perform(new Date(2021, 4, 25, 8, 17, 39, 285))
     const [b] = timestamp.perform(new Date(2021, 4, 25, 8, 54, 39, 285))
 
@@ -49,7 +49,7 @@ describe('Comparisons', () => {
   })
 
   test('that two dates in different seconds of the same minute compare correctly', () => {
-    const timestamp = new Timestamp()
+    const timestamp = new UTCTimestamp()
     const [a] = timestamp.perform(new Date(2021, 4, 25, 8, 17, 39, 285))
     const [b] = timestamp.perform(new Date(2021, 4, 25, 8, 17, 40, 285))
 
@@ -57,7 +57,7 @@ describe('Comparisons', () => {
   })
 
   test('that two dates in different fractions of the same second compare correctly', () => {
-    const timestamp = new Timestamp()
+    const timestamp = new UTCTimestamp()
     const [a] = timestamp.perform(new Date(2021, 4, 25, 8, 17, 40, 100))
     const [b] = timestamp.perform(new Date(2021, 4, 25, 8, 17, 40, 285))
 
@@ -67,14 +67,14 @@ describe('Comparisons', () => {
 
 describe('Query analysis', () => {
   test('equal-to', () => {
-    const timestamp = new Timestamp()
+    const timestamp = new UTCTimestamp()
     const [tuple] = timestamp.performForQuery("==", new Date(2021, 4, 25, 8, 17, 40, 100))
 
     expect(tuple).toEqual(Buffer.from("00000179a0748004", "hex"))
   })
 
   test('greater-than or equal-to', () => {
-    const timestamp = new Timestamp()
+    const timestamp = new UTCTimestamp()
     const [tuple] = timestamp.performForQuery(">=", new Date(2021, 4, 25, 8, 17, 40, 100))
 
     expect(tuple).toEqual([
@@ -84,7 +84,7 @@ describe('Query analysis', () => {
   })
 
   test('greater-than', () => {
-    const timestamp = new Timestamp()
+    const timestamp = new UTCTimestamp()
     const [tuple] = timestamp.performForQuery(">", new Date(2021, 4, 25, 8, 17, 40, 100))
 
     expect(tuple).toEqual([
@@ -94,7 +94,7 @@ describe('Query analysis', () => {
   })
 
   test('less-than or equal-to', () => {
-    const timestamp = new Timestamp()
+    const timestamp = new UTCTimestamp()
     const [tuple] = timestamp.performForQuery("<=", new Date(2021, 4, 25, 8, 17, 40, 100))
 
     expect(tuple).toEqual([
@@ -104,7 +104,7 @@ describe('Query analysis', () => {
   })
 
   test('less-than', () => {
-    const timestamp = new Timestamp()
+    const timestamp = new UTCTimestamp()
     const [tuple] = timestamp.performForQuery("<", new Date(2021, 4, 25, 8, 17, 40, 100))
 
     expect(tuple).toEqual([
@@ -114,7 +114,7 @@ describe('Query analysis', () => {
   })
 
   test('between', () => {
-    const timestamp = new Timestamp()
+    const timestamp = new UTCTimestamp()
     const a = new Date(2021, 4, 25, 8)
     const b = new Date(2021, 4, 25, 9)
     const [tuple] = timestamp.performForQuery("><", [a, b])
